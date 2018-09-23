f97b86766b51516c7e7123b5c356f7e980d48b0f
John D. Fisher <jdfenw@gmail.com>
2018-09-23 09:53:53 -0700

    chore(link_filter): apply black

--- link_filter.py
+++ link_filter.py
@@ -3,8 +3,6 @@
 
     Convert .md extension to .pdf or .html, depending on output format.
 
-    Convert .sh3d to output extension.
-
     Usage:
         pandoc --filter=link_filter.py
 """
@@ -12,8 +10,9 @@
 from os.path import splitext
 from urllib.parse import urlparse
 
-import sys
+# import sys
 import panflute as pf
+
 
 def action(elem, doc):
     """Convert URL extensions to correct extension for output format"""
@@ -21,21 +20,20 @@ def action(elem, doc):
     #                    ('html', '.html'),
     #                    ('latex', '.pdf')]
     #                  )
-    docformats = dict([('html5', ''),
-                       ('html', '.html'),
-                       ('latex', '.pdf')]
-                     )
-    extensions = ('.sh3d', '.md')
+    docformats = dict([("html5", ""), ("html", ".html"), ("latex", ".pdf")])
+    extensions = ".md"
+    # extensions = ('.sh3d', '.md')
     if isinstance(elem, pf.Link) and elem.url.endswith(extensions):
         if doc.format in docformats:
             extension = splitext(urlparse(elem.url).path)[1]
             # pf.debug(docformats[doc.format])
-            elem.url = elem.url[:-len(extension)] + docformats[doc.format]
+            elem.url = elem.url[: -len(extension)] + docformats[doc.format]
             # pf.debug(elem.url)
             return elem
 
     return None
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # pf.debug(str(sys.argv))
     pf.run_filter(action)
